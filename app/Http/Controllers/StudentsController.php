1a971d920242c68b45ba62ac627d95972f15f460
rerereanl <renaldi232701@gmail.com>
2021-11-03 14:19:52 +0700

    5 finish

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Echo_;
+
+use function PHPSTORM_META\elementType;
 
 class StudentsController extends Controller
 {
     function index(){
-        echo "Test";
+        $students = Student::all();
+
+        if (count($students) > 0){
+            $data = [
+                'message' => 'Get all students',
+                'resource' => $students
+            ];
+            return response()->json($data,200);
+
+        }
+        else {
+            $data = [
+                'message' => 'Data not found'
+            ];
+            return response()->json($data,404);
+        }
     }
 }
