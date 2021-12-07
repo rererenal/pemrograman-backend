dda660402aa542d0a72abf8a653c9b3481ba835a
Rere Renaldi <70957925+rererenal@users.noreply.github.com>
2021-12-07 15:14:59 +0700

    Add files via upload

---
+++ js/data/fruit.js
@@ -0,0 +1,13 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+
+
+const fruits = ["Apel", "Belimbing", "Durian", "Nangka", "Nanas", "Salak"];
+
+
+
+ // TODO 2: export variable fruits
+ module.exports = fruits;
